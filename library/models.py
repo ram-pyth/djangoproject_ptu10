a5003c09bff2187427a4a24a51e96d3c45c684f0
Ramūnas Pl <ramunas.platukis@gmail.com>
2023-04-12 17:01:33 +0300

    Author model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField("Vardas", max_length=100)
+    last_name = models.CharField("Pavardė", max_length=100)
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"

8d09ab98bfebcc9f2950e605da114d66f8dbd11c
Ramūnas Pl <ramunas.platukis@gmail.com>
2023-04-17 10:13:56 +0300

    4-a paskaita done, base.html + css

--- djangoproject_ptu10/urls.py
+++ djangoproject_ptu10/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("library/", include("library.urls")),
-    path("", RedirectView.as_view(url="library/", permanent=True))
-]
+    path("", RedirectView.as_view(url="library/", permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

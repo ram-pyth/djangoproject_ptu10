d5ee9dde52971cebf0b6414691d9b9ba71769cee
Ramūnas Pl <ramunas.platukis@gmail.com>
2023-04-17 09:49:53 +0300

    4-a paskaita, base.html

--- djangoproject_ptu10/urls.py
+++ djangoproject_ptu10/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("library/", include("library.urls"))
+    path("library/", include("library.urls")),
+    path("", RedirectView.as_view(url="library/", permanent=True))
 ]
